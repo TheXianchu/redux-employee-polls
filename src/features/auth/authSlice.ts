46353391cd47e36358ad65a8589f6b563f919799
Darryll de Quillettes <darryll@interactivestudios.nl>
2022-08-16 09:12:23 +0200

    feat: combine reducers

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,6 +1,6 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { _getUsers } from "../../../_DATA";
-import {User} from "../../types/User";
+import {  createSlice } from '@reduxjs/toolkit';
+import { User } from "../../types/User";
+import { store } from "../../app/store";
 
 export interface AuthState {
     userId?: string;
@@ -12,31 +12,16 @@ const initialState: AuthState = {
     authenticated: false,
 };
 
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
-export const authenticateAsync = createAsyncThunk(
-    'authenticate',
-    async (userId: string) => {
-        const response = await _getUsers();
-
-        return response.find((user: User) => user.id === userId);
-    }
-);
-
 export const authSlice = createSlice({
     name: 'auth',
     initialState,
     // The `reducers` field lets us define reducers and generate associated actions
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(authenticateAsync.fulfilled, (state, action) => {
-            // Add user to the state array
-            state.authenticated = action.payload;
-        })
-    }
+    reducers: {
+        authenticate: (state) => {
+            const { users } = store.getState();
+            state.authenticated = users.users.some((user: User) => user.id === state.userId);
+        }
+    },
 });
 
 export const { } = authSlice.actions;
